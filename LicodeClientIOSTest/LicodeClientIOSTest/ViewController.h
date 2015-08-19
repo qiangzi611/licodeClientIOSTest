652d38828d832e08027503859e14c2976273b257
qiangzi611 <815608542@qq.com>
2015-08-19 16:05:28 +0800

    创建一个LicodeClientIOS工程测试

---
+++ LicodeClientIOSTest/LicodeClientIOSTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LicodeClientIOSTest
+//
+//  Created by zhaoqiang on 15/8/19.
+//  Copyright (c) 2015年 YZS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LicodeClientIOSTest/LicodeClientIOSTestTests/LicodeClientIOSTestTests.m
@@ -0,0 +1,40 @@
+//
+//  LicodeClientIOSTestTests.m
+//  LicodeClientIOSTestTests
+//
+//  Created by zhaoqiang on 15/8/19.
+//  Copyright (c) 2015年 YZS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LicodeClientIOSTestTests : XCTestCase
+
+@end
+
+@implementation LicodeClientIOSTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
